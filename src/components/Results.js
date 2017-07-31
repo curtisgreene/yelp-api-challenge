52f9de0e43b3ea737e3bfa10b0d7ef8eafd1e4a5
curtisgreene <curtis.j.w.greene@gmail.com>
2017-07-31 11:23:30 -0400

    installed semantic and added beginning components

---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Results(props) {
+
+  return (
+    <div>
+      <h5>Results componet</h5>
+    </div>
+  )
+}
+
+
+export default Results
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default class Search extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <h3>Search Bar Component</h3>
+      </div>
+    )
+  }
+}
